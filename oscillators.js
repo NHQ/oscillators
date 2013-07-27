b9d3d6403ef2dd795b2a41722fe1aac6e117dce7
Johnnyscriptus <mostmodernist@gmail.com>
2013-07-26 23:45:27 -0700

    simple fied

--- oscillators.js
+++ oscillators.js
@@ -1,61 +1,55 @@
-module.exports = function (){
+var OZ = module.exports
+var tau = Math.PI * 2
 
-    var OZ = {}
-    , tau = Math.PI * 2
+OZ.sine = sine;
+OZ.saw = saw;
+OZ.saw_i = saw_i;
+OZ.triangle = triangle;
+OZ.triangle_s = triangle_s;
+OZ.square = square;
 
-    OZ.sine = sine;
-    OZ.saw = saw;
-    OZ.saw_i = saw_i;
-    OZ.triangle = triangle;
-    OZ.triangle_s = triangle_s;
-    OZ.square = square;
+function sine(t, f){
 
-    return OZ
+    return Math.sin(t * tau * f);
+    
+};
 
-    function sine(t, f){
+function saw(t, f){
 
-			return Math.sin(t * tau * f);
-	
-    };
+    var n = ((t % (1/f)) * f) % 1; // n = [0 -> 1]
 
-    function saw(t, f){
+    return -1 + (2 * n)
 
-			var n = ((t % (1/f)) * f) % 1; // n = [0 -> 1]
+};
 
-			return -1 + (2 * n)
+function saw_i(t, f){
 
-    };
+    var n = ((t % (1/f)) * f) % 1; // n = [0 -> 1]
+    
+    return 1 - (2 * n)
 
-    function saw_i(t, f){
+};
 
-			var n = ((t % (1/f)) * f) % 1; // n = [0 -> 1]
-	
-			return 1 - (2 * n)
+function triangle(t, f){
+    
+    var n = ((t % (1/f)) * f) % 1; // n = [0 -> 1]
+    
+    return n < 0.5 ? -1 + (2 * (2 * n)) : 1 - (2 * (2 * n))
+    
+};
 
-    };
+function triangle_s(t, f){
+    
+    var n = ((t % (1/f)) * f) % 1; // n = [0 -> 1]
+    
+    var s = Math.abs(Math.sin(t));
+    
+    return n < s ? -1 + (2 * (2 * (n / s))) : 1 - (2 * (2 * (n / s)))
+    
+};
 
-    function triangle(t, f){
-	
-			var n = ((t % (1/f)) * f) % 1; // n = [0 -> 1]
-	
-			return n < 0.5 ? -1 + (2 * (2 * n)) : 1 - (2 * (2 * n))
-	
-    };
+function square(t, f){
 
-    function triangle_s(t, f){
-	
-			var n = ((t % (1/f)) * f) % 1; // n = [0 -> 1]
-	
-			var s = Math.abs(Math.sin(t));
-	
-			return n < s ? -1 + (2 * (2 * (n / s))) : 1 - (2 * (2 * (n / s)))
-	
-    };
-
-     function square(t, f){
-
-			return ((t % (1/f)) * f) % 1 > 0.5 ? 1 : -1;
-
-    };
+    return ((t % (1/f)) * f) % 1 > 0.5 ? 1 : -1;
 
 };
