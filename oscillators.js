d6739f2e0a92267f12fa1ffbeb7c23bed013ce13
johnnyscript <mostmodernist@gmail.com>
2016-04-30 21:31:11 -0700

    added sigmoid sine

--- oscillators.js
+++ oscillators.js
@@ -7,6 +7,9 @@ OZ.saw_i = saw_i;
 OZ.triangle = triangle;
 OZ.triangle_s = triangle_s;
 OZ.square = square;
+OZ.sig = sig
+OZ.sq = square
+OZ.tri = triangle
 
 function sine(t, f) {
 
@@ -53,3 +56,8 @@ function square(t, f) {
     return ((t % (1 / f)) * f) % 1 > 0.5 ? 1 : -1;
 
 };
+
+function sig(t, f){
+  return 1 / (1 + Math.pow(Math.E, (t * Math.PI * 2 * f) % 12))
+}
+
